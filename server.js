220ab5a2a05c25576ac58d51b9ed243e9152f7c9
Mandeep Sai <63707885+Mandeep-Sai@users.noreply.github.com>
2020-09-16 23:49:51 +0200

    socket skeleton added

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const tweetRoutes = require("./tweets");
 const profileRoutes = require("./profiles");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const http = require("http");
+const socketio = require("socket.io");
 dotenv.config();
 
 const whitelist = ["http://localhost:3000"];
@@ -20,11 +22,21 @@ const corsOptions = {
 };
 
 const server = express();
+const socketServer = express();
+const app = http.createServer(socketServer);
+
+const io = socketio(app);
 server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(cors(corsOptions));
 server.use("/tweets", tweetRoutes);
 server.use("/profiles", profileRoutes);
+
+//socket
+
+io.on("connection", (socket) => {
+  console.log("socketId", socket.id);
+});
 
 const url =
   "mongodb+srv://user7:user@community.hw7hj.mongodb.net/community?retryWrites=true&w=majority";
@@ -40,4 +52,7 @@ mongoose
   );
 mongoose.connection.on("connected", () => {
   console.log("connected to atlas");
+});
+app.listen(3004, () => {
+  console.log("sockets running on 3004");
 });
